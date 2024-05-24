4aa7a461147f64e6f53af6e5736d97c86803b97f
juanib99 <juanignaciobisello@hotmail.com>
2024-05-24 19:33:50 -0300

    +

--- src/components/Presentation.jsx
+++ src/components/Presentation.jsx
@@ -4,8 +4,8 @@ import { Badge } from './Badge'
 export const Presentation = () => {
   return (
     <>
-        <h1>Hi! <Badge /></h1>
-        <h2></h2>
+        <h1>Hi! I'm Ignacio <Badge /></h1>
+        <h2>Web designer and developer specialized in React, based in Buenos Aires, Argentina.</h2>
     </>
     
   )
