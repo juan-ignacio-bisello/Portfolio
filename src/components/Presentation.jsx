300fec07827d6b282ce24fda5c9cfb5eef438b07
juanib99 <juanignaciobisello@hotmail.com>
2024-05-23 23:30:20 -0300

    adding font

---
+++ src/components/Presentation.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Badge } from './Badge'
+
+export const Presentation = () => {
+  return (
+    <>
+        <h1>Hi! <Badge /></h1>
+        <h2></h2>
+    </>
+    
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react' ;
 import ReactDOM from 'react-dom/client' ;
 import './index.css' ;
 import { Header } from './components/Header.jsx' ;
+import '@fontsource-variable/onest';
+import { Presentation } from './components/Presentation.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <Header class='flex justify-center' />
+    <Presentation />
   </React.StrictMode>,
 )
